9ed6dcbe1424fe7fc2415b11a328899e325d1ff8
RB <rb123456>
2020-10-19 18:14:18 -0700

    More files added

---
+++ DollarDay/Art1/src/test/java/DollarDaysTest.java
@@ -0,0 +1,56 @@
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+import org.testng.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DollarDaysTest {
+  @Test
+  public void f() {
+	  System.setProperty("webdriver.chrome.driver", "D://Ritu//Testing Training//Selenium//chromedriver_win32//chromedriver.exe");
+	  WebDriver driver = new ChromeDriver();
+      driver.get("http://www.dollardays.com");
+      //driver.close();
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+  @BeforeSuite
+  public void beforeSuite() {
+  }
+
+  @AfterSuite
+  public void afterSuite() {
+  }
+
+}
